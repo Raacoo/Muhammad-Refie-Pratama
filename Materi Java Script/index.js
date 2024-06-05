cde622a7ae641ed8317e03f37cc2dcaf2db4efa4
Raacoo <muhammadrefiepratama.2ib@gmail.com>
2024-06-05 19:09:43 +0700

    Tugas 3
    Tugas 3 ; Muhamad Refie Pratama // 3 DIalog

---
+++ Materi Java Script/index.js
@@ -0,0 +1,20 @@
+// // 1. Clg (console.log)
+// console.log("Hello World");
+// //2. InnerHTML()
+// documentgetElementById("demo").innerHTML = "Test12345";
+
+// // var, let, conts
+//     // var
+// let buttonSubmit = 2+4;
+// console.log(buttonSubmit);
+// buttonSubmit = "7";
+// console.log(buttonSubmit);
+
+// const data = 1+1
+// console.log(data);
+
+
+function buttonSubmit(){
+    alert("Data sudah terkirim✔");
+    alert("Data sudah diterima✔");
+}
